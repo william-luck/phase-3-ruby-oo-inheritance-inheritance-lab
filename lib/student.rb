dba0b9c19ea0ddcf446d88f30438354f94b19375
William Luck <luck.william.h@gmail.com>
2022-08-09 11:45:52 -0500

    commit tree 587a40731dc012b6d00934c9c337a5ba3f95deec

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
 
+require 'pry'
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
+
+binding.pry
+0
